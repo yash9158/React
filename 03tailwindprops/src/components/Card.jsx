e3c4cb6ee7a2b3eedf5e79d4f8b096bd6028c094
Yash Kumar <yashkumar9158@gmail.com>
2025-01-22 15:57:17 +0530

    tailwindreact

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function card({content,description="default"}) {
+
+  return (
+    <div className="w-60 h-60 rounded-xl">
+        <img
+          src="https://media1.giphy.com/media/z8n8dWgQ0mgEIyzlmV/giphy.gif?cid=790b7611a5ba988db1bc7457636dd163c28af6f6dbc84a77&rid=giphy.gif&ct=g"
+          alt="Giphy"
+          className="rounded-t-xl"
+        />
+        <div className="glass py-4 px-5 relative -top-[3.4rem]  rounded-b-xl z-10">
+          <h1 className="font-bold  font-mono  text-xl">{content} || {description}</h1>
+        </div>
+      </div>
+  )
+}
+
+export default card
